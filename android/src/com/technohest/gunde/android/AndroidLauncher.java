cf87c1f2a1f490374004014239ccdcb5dec91c85
oskar367 <oskar.jedvert@gmail.com>
2015-06-24 14:47:09 +0200

    initial commit

---
+++ android/src/com/technohest/gunde/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.technohest.gunde.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.technohest.gunde.GundeMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GundeMain(), config);
+	}
+}
